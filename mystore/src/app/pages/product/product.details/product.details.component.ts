b9b9d7538a71f23de4737070ac15549ab6de1f89
ajoy <ajoymahato16@gmail.com>
2025-05-03 14:11:41 +0530

    details page work done

--- mystore/src/app/pages/product/product.details/product.details.component.ts
+++ mystore/src/app/pages/product/product.details/product.details.component.ts
@@ -1,14 +1,17 @@
 
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component,OnInit, ViewChild, ElementRef, AfterViewInit, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router'; 
 import { Observable } from 'rxjs';
 import { State, Store } from '@ngxs/store';
 import { ProductState } from '../../../state/state.product'; // Import the Product interface from the state file
+import e from 'express';
 
 @Component({
   selector: 'app-product.details',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule,FormsModule],
   templateUrl: './product.details.component.html',
   styleUrl: './product.details.component.css'
 })
@@ -17,6 +20,9 @@ export class ProductDetailsComponent implements OnInit {
   // This component is responsible for displaying product details
     selectedImage: string = ''; // Variable to hold the selected image URL
     product:any = {}; // Initialize an empty product object to hold product details
+    strokeprice:number = 0; // Variable to hold the price after discount
+    quntity: number = 1; // Variable to hold the quantity of the product
+    quntityerror:boolean = false;
 
 
   constructor(private routes:ActivatedRoute, private store: Store) { } // Constructor for the component, currently empty
@@ -25,35 +31,44 @@ export class ProductDetailsComponent implements OnInit {
   @ViewChild('mainImage') mainImageElement!: ElementRef; // ViewChild to access the main image element in the template
  
   ngAfterViewInit() {
-      // This lifecycle hook is called after the view has been initialized
-      if (this.mainImageElement) {
-        this.mainImageElement.nativeElement.src = this.selectedImage; // Set the initial image source to the selected image
-      } else {
-        console.error('Main image element is not available.');
-      }
+    
   }
 
   ngOnInit() {
 
      const product_id = this.routes.snapshot.params['id']; // Get the product ID from the route parameters
-
-    console.log('id:', product_id); // Log the product ID to the console for debugging purposes
-    console.log('state:',this.store.selectSnapshot(ProductState.getProductList)); // Log the current product list state to the console
-
-    this.product = this.store.selectSnapshot(ProductState.getProductList).find((product) => product.id == product_id); // Find the product in the state using the ID
-  
-    console.log('product:',this.product); // Log the product details to the console for debugging purposes
-    this.selectedImage = this.product.images[0];
-    // Check if the product is found and log its details
-
+      this.product = this.store.selectSnapshot(ProductState.getProductList).find((product) => product.id == product_id); // Find the product in the state using the ID
+      this.selectedImage = this.product.images[0];
+      // Check if the product is found and log its details
+      console.log('Product found:', this.product);
+      this.strokeprice = this.getPricewithDiscount(this.product.price, this.product.discountPercentage);
   }
 
-  changeImage(image: string): void {
-    if (this.mainImageElement) {
-      this.mainImageElement.nativeElement.src = image; // Update the image source
-    } else {
-      console.error('Main image element is not available.');
+  roundRating(value: number): number {
+    return Math.round(value);
+  }
+  getPricewithDiscount(price: number, discount: number): number {
+    return Math.round(price + (price * discount) / 100); // Calculate the price after applying the discount
+  }
+
+    validateInput()
+    {
+        if(this.quntity > this.product.stock)
+        {
+          
+          console.log('out of stock',this.quntityerror);
+
+        }else{
+          
+          console.log('In Stock',this.quntityerror);
+      }
     }
-  }
+
+    addToCart()
+    {
+      
+    }
+
+
 
 }
